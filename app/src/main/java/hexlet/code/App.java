21ae57a60f314f227c152d3c05dfbfff666f19e9
Evgenii <evg.sneg.117@yandex.ru>
2026-02-11 11:20:07 +1000

    add picocli support to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,38 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+import hexlet.code.Differ;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    private String filePath1;
+
+    private String filePath2;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.out.println("Hello, World!");
+
+        var diff = Differ.generate(filePath1, filePath2);
+        System.out.println(diff);
+
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
