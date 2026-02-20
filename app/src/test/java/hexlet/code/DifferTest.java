a197a6a02fe2b126408979810ba672978e069490
Evgenii <evg.sneg.117@yandex.ru>
2026-02-20 14:58:15 +1000

    update DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,4 +1,57 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class DifferTest {
+
+    public static Path getFixturePath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+    }
+
+    public static String readFixture(String fileName) throws Exception {
+        var path = getFixturePath(fileName);
+        return Files.readString(path).trim();
+    }
+
+    @Test
+    public void generateTest() throws Exception {
+        var actual = Differ.generate(getFixturePath("file1.json").toString(),
+                getFixturePath("file2.json").toString());
+        var expected = readFixture("Right.txt");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void missingTest() throws Exception {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Differ.generate(getFixturePath("file1.json").toString(), null);
+        });
+    }
+
+    @Test
+    public void emptyTest() throws Exception {
+        var actual = Differ.generate(getFixturePath("file1.json").toString(),
+                getFixturePath("emptyFile.json").toString());
+        var expected = readFixture("EmptyRight.txt");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sameFileTest() throws Exception {
+        var actual = Differ.generate(getFixturePath("file1.json").toString(),
+                getFixturePath("file1.json").toString());
+        var expected = readFixture("SameRight.txt");
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
