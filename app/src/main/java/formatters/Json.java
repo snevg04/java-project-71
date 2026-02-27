81e7250b44be14773e3e68596aa354ddc16956b4
Evgenii <evg.sneg.117@yandex.ru>
2026-02-27 11:11:37 +1000

    add new formatter Json.java

--- app/src/main/java/formatters/Json.java
+++ app/src/main/java/formatters/Json.java
@@ -1,4 +1,44 @@
 package formatters;
 
-public class Json {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hexlet.code.Builder;
+import hexlet.code.Differ;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Json implements Builder {
+
+    public String build(List<Differ.DiffEntry> entries) throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(getJsonList(entries));
+    }
+
+    private static List<Differ.DiffEntry> getJsonList(List<Differ.DiffEntry> entries) {
+
+        List<Differ.DiffEntry> jsonEntries = new ArrayList<>();
+
+        for (var entry : entries) {
+            Differ.DiffEntry jsonEntry;
+
+            if ("updated".equals(entry.getStatus())) {
+                jsonEntry = new Differ.DiffEntry(entry.getKey(), entry.getStatus(),
+                        entry.getValue(), entry.getNewValue());
+            } else {
+                jsonEntry = new Differ.DiffEntry(entry.getKey(), entry.getStatus(),
+                        entry.getValue(), null);
+            }
+
+            jsonEntries.add(jsonEntry);
+        }
+
+        return jsonEntries.stream()
+                .sorted(Comparator.comparing(Differ.DiffEntry::getKey))
+                .toList();
+    }
 }
