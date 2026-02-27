942128952f2167dde2d46ba81b1188b6c6d2eb8d
Evgenii <evg.sneg.117@yandex.ru>
2026-02-27 11:12:29 +1000

    update Builder

--- app/src/main/java/hexlet/code/Builder.java
+++ app/src/main/java/hexlet/code/Builder.java
@@ -1,7 +1,11 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.List;
+
 public interface Builder {
 
-    String build(Differ.DiffEntry entry);
+    String build(List<Differ.DiffEntry> entries) throws JsonProcessingException;
 
 }
